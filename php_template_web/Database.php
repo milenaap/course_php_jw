d96a9082a252ef23038386fe328a880c38809d82
Milena <darimile@gmail.com>
2026-02-03 22:20:32 +0100

    add Database

---
+++ php_template_web/Database.php
@@ -0,0 +1,32 @@
+
+<?php
+
+class Database
+{
+
+    public $connection; // Se declara la variable
+
+    
+    public function __construct($config, $username = 'phpuser', $password = 'phppass'){
+
+        
+        $dns = 'mysql:' . (http_build_query($config, '', ';'));
+       
+
+        // $dns = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']};charset={$config['charset']}";
+        $this->connection = new PDO($dns, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+    }
+
+
+    public function query($query)
+    {
+
+        $statement = $this->connection->prepare($query);
+        
+        $statement->execute();
+
+        return $statement;
+    }
+}
