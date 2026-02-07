1ccfa36ebab8108464e06c20184e824416fcde01
Milena <darimile@gmail.com>
2026-02-07 22:06:31 +0100

    Refactor Authenticator

--- php_template_web/Http/Forms/LoginForm.php
+++ php_template_web/Http/Forms/LoginForm.php
@@ -26,4 +26,8 @@ class LoginForm
     public function errors(){
         return $this->errors;
     }
+
+    public function error($field, $message){
+        $this->errors[$field] = $message;
+    }
 }
